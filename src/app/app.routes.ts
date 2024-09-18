db3965070e54deaaed64db9c44e827ca3bdc48be
jowel <jowelpana44@gmail.com>
2024-09-18 09:44:54 +0800

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../nav/home/home.component';
+import { ContactsComponent } from '../nav/contacts/contacts.component';
+import { AnnouncementComponent } from '../nav/announcement/announcement.component';
+import { AboutUsComponent } from '../nav/about-us/about-us.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/landing-page',
+    pathMatch: 'full',
+  },
+  {
+    path: 'landing-page',
+    component: LandingPageComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'about',
+        component: AboutUsComponent,
+      },
+      {
+        path: 'contacts',
+        component: ContactsComponent,
+      },
+      {
+        path: 'announcement',
+        component: AnnouncementComponent,
+      },
+    ],
+  },
+];
